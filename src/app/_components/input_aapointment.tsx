7ed95664b2af351138913b90c474d0ce92dd051d
mustafa laptop <ad16mkha@uwcad.it>
2024-08-26 16:38:11 -0400

    remove underline

--- src/app/_components/input_aapointment.tsx
+++ src/app/_components/input_aapointment.tsx
@@ -27,7 +27,7 @@ export default function InputAppointment() {
   }
   return (
     <div>
-        {response && <div className="decoration-solid text-xl text-lime-300 underline">{response}</div>}
+        {response && <div className="decoration-solid text-xl text-lime-300">{response}</div>}
         <div className="grid w-full gap-2">
             <Textarea onChange={(e) => {setText(e.target.value)}} className='bg-black' placeholder="Ask for an appointment at your convenient time ..." />
             <Button onClick={(e) => handleSend(text)}>Send message</Button>
